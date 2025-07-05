3cdad320c2f5aacccf9a2178d801c32eaf6be4f7
CMEIRELES <caiomeirles44@gmail.com>
2025-07-04 22:48:42 -0300

    Corrige código e faz a soma alem de encontrar o maior

--- vetor.cpp
+++ vetor.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> numeros(n);
+    int soma = 0;
+    int maior;
+
+    for (int i = 0; i < n; i++) {
+        cin >> numeros[i];
+        soma += numeros[i];
+        if (i == 0 || numeros[i] > maior) {
+            maior = numeros[i];
+        }
+    }
+
+    cout << "Maior: " << maior << endl;
+    cout << "Soma: " << soma << endl;
+
+    return 0;
+}
+#include <iostream>
 using namespace std;
 
 int main() {
