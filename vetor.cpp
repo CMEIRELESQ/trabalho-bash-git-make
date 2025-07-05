a531e8b865ffdecb531619c2939cea05b10c638f
CMEIRELESQ <109631299+CMEIRELESQ@users.noreply.github.com>
2025-07-04 22:50:03 -0300

    Update vetor.cpp
    correçao de erros e adição da soma

--- vetor.cpp
+++ vetor.cpp
@@ -23,17 +23,3 @@ int main() {
 
     return 0;
 }
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    int maior, x;
-    for (int i = 0; i < n; i++) {
-        cin >> x;
-        if (i == 0 || x > maior) maior = x;
-    }
-    cout << maior << endl;
-    return 0;
-}
